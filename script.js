c2ad7e3e5d37d6ac1b6245cf71938e6cf64eaef1
Hemi Yudha <hemiyudha@gmail.com>
2024-06-13 14:49:02 +0700

    Commit pertama

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+console.log("Script Started");
 
+let quotecontent="Banyak Bicara, Banyak Bekerja"
+let quoteauthor ="Bung Karno"
+let namaku ="Hemi Yudha Muhammad"
+
+function prevQuote(){
+    console.log("prevQuote");
+    let containercontentid= document.getElementsById("containercontentid");
+
+    setTimeout(function (){
+        containercontentid.remove();}, 3000);
+    }
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Aku Dipanggil")
+    document.getElementById("namaku").innerText=namaku;
+})
+
+//DOM : Document Object Model -> HTML
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Aku Dipanggil");
+    document.getElementById("quotecontent").innerText=quotecontent;
+    document.getElementById("quoteauthor").innerText=quoteauthor;
+})
